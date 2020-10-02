dc4be8fd418c1c795030e30d763e18d7635450ff
MollyZalman <68440011+MollyZalman@users.noreply.github.com>
2020-10-02 10:36:39 -0400

    Deleting consoles and creating createPhrase

--- js/Game.js
+++ js/Game.js
@@ -9,7 +9,23 @@
          this.activePhrase = null;
      }
  
- 
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases() {
+        const phrases = [
+            new Phrase("Eat, drink, and be scary"),
+            new Phrase("Trick or treat"),
+            new Phrase("Ghouls just want to have fun"),
+            new Phrase("This is Halloween"),
+            new Phrase("Hocus Pocus"),
+            new Phrase("I put a spell on you"),
+            new Phrase("Pick your poisen"),
+            new Phrase("Witch, please"),
+        ];
+        return phrases;
+    };
  
  
  
